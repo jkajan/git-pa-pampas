c20f564e6ba0e3a0b736ef794ed9ea7f171b3ec6
jkajan <jkajan@github.com>
2019-11-25 15:32:14 +0200

    Fix teel skripte na liite

---
+++ kör.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+csvfil="./kodourd.csv"
+while IFS=, read -r kommando oul
+do
+	if [[ $oul = *[![:ascii:]]* ]]; then
+		echo git $kommando "\$@" > /bin/git-$oul;
+		chmod +x /bin/git-$oul;
+		echo -e 'bin: \t'$kommando == $oul;
+	else
+		sudo -u $SUDO_USER git config --global alias.$oul $kommando;
+		echo -e 'alias: \t'$kommando == $oul;
+	fi
+done < $csvfil
